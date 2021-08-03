2d2632a6c0e339a9f819d26a5e3ec8ad597e59f6
ja-odur <odurjoseph8@gmail.com>
2021-08-04 02:11:25 +0300

    create mouth

---
+++ Mouth.js
@@ -0,0 +1,12 @@
+import {arc} from "d3";
+import React from "react";
+
+export const Mouth = ({ mouthRadius, mouthWidth }) => {
+    const mouthArc = arc()
+    .innerRadius(mouthRadius)
+    .outerRadius(mouthRadius + mouthWidth)
+    .startAngle(Math.PI / 2)
+    .endAngle(Math.PI * 3/2);
+
+    return <path d={mouthArc()} />
+}
